9acad08e6fc2b29a229862f3c278d9aa0ec141e4
codebabel-appbag <codebabel.mint@gmail.com>
2026-09-15 13:21:40 -0300

    Update royalprint.lua

--- src/royalprint.lua
+++ src/royalprint.lua
@@ -1,20 +1,18 @@
 
 local royalprint = {
-    go = function go(x) print(x) end
-    log = function log(x) print(x) end
-    show = function show(x) print(x) end
-    echo = function echo(x) print(x) end
-    puts = function puts(x) print(x) end
-    display = function display(x) print(x) end
-    putline = function putline(x) print(x) end
-    cout = function cout(x) print(x) end
-    printf = function printf(x) print(x) end
-    printin = function printin(x) print(x) end
-    println = function println(x) print(x) end
-    xLapis = function xLapis(x) print(x) end
-    xLua = function xLua(x) print(x) end
-    codebabel = function codebabel(x) print(x) end
-    devdalua = function devdalua(x) print(x) end
+    go = function (x) print(x) end,
+    log = function (x) print(x) end,
+    show = function (x) print(x) end,
+    echo = function (x) print(x) end,
+    puts = function (x) print(x) end,
+    display = function (x) print(x) end,
+    putline = function (x) print(x) end,
+    cout = function (x) print(x) end,
+    printf = function (x) print(x) end,
+    printin = function (x) print(x) end,
+    println = function (x) print(x) end,
+    xLapis = function (x) print(x) end,
+    codebabel = function (x) print(x) end
 }
 
 for name, func in pairs(royalprint) do
